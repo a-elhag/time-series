57baa8b5b8a23452ca93dfd25dfd0ba789c4b481
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-10-29 19:52:17 -0400

    added links

--- seasonality.py
+++ seasonality.py
@@ -10,10 +10,13 @@ data_period_avg = np.zeros((4,1))
 for period in range(4):
     data_period_avg[period] = data[period::4].mean()
 
-data_table = np.hstack((np.arange(4).reshape(4,-1), data.reshape(4,3),
-                       data_period_avg.reshape(4,-1)))
+data_period_index = data_period_avg/data_period_avg.mean()
 
-headers = ["Period", "Cycle 1", "Cycle 2", "Cycle 3", "Average"]
+data_table = np.hstack((np.arange(4).reshape(4,-1), data.reshape(4,3),
+                       data_period_avg.reshape(4,-1),
+                       data_period_index.reshape(4, -1)))
+
+headers = ["Period", "Cycle 1", "Cycle 2", "Cycle 3", "Average", "Index"]
 table = tabulate(data_table, headers, tablefmt="fancy_grid")
 print(table)
 
