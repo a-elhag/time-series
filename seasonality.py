430277c67c5b6e92ca33bcd770acb22b5aaedcf2
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-10-29 20:00:29 -0400

    finished

--- seasonality.py
+++ seasonality.py
@@ -1,7 +1,9 @@
+## Part 0: Initializing
 from tabulate import tabulate
 import matplotlib.pyplot as plt
 import numpy as np
 
+## Part 1: Data Manipulation
 data = np.array([5384, 8081, 10282, 9156, 6118, 9139, 12460, 10717, 7825,
              9693, 15177, 10990])
 
@@ -19,6 +21,19 @@ data_table = np.hstack((np.arange(4).reshape(4,-1), data.reshape(4,3),
 headers = ["Period", "Cycle 1", "Cycle 2", "Cycle 3", "Average", "Index"]
 table = tabulate(data_table, headers, tablefmt="fancy_grid")
 print(table)
+
+data_period_index = data_period_index.reshape(4, 1)
+## Part 2: Finish him!
+data = data.reshape(-1, 1)
+data_noseason = data / np.tile(data_period_index, (3, 1))
+
+## Part 3: Plotting
+plt.plot(data, label="Original Data")
+plt.plot(data_noseason, label="No Season")
+plt.legend()
+plt.show()
+
+
 
 
 
